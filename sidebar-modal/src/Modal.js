2d0f9b9475b1702fb1b1c34b5bea6c77893a9cc3
Lee Hyungi <hyungi.lee.622@gmail.com>
2021-04-17 19:47:04 +0900

    feat: Complete sidebar and modal project in reactjs

--- sidebar-modal/src/Modal.js
+++ sidebar-modal/src/Modal.js
@@ -1,12 +1,14 @@
-import React, { useState } from 'react'
-import { FaTimes } from 'react-icons/fa'
+import React, { useState } from 'react';
+import { FaTimes } from 'react-icons/fa';
+import { useGlobalContext } from './context';
+
 const Modal = () => {
-  
+  const { isModalOpen, closeModal } = useGlobalContext();
   return (
-    <div class={`modal-overlay`}>
+    <div className={`${isModalOpen ? 'modal-overlay show-modal' : 'modal-overlay'}`}>
       <div className="modal-container">
         <h3>modal content</h3>
-        <button className='close-modal-btn'>
+        <button className='close-modal-btn' onClick={closeModal}>
           <FaTimes />
         </button>
       </div>
